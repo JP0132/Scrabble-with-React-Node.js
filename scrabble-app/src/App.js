87a8c3a8f63ddb6b61a0abcffb32c7dd73f37455
Jaynik Parsotomo <jp539@student.le.ac.uk>
2021-11-27 20:36:42 +0000

    Add the components board and rack into the app + css to centre the components

--- scrabble-app/src/App.js
+++ scrabble-app/src/App.js
@@ -1,9 +1,15 @@
-
 import './App.css';
+import Board from './Components/Board';
+import Rack from './Components/Rack';
 
 function App() {
   return (
     <div className="App">
+      <h1 className="appTitle">AIECHO SCRABBLE</h1>
+      <div className="centre">
+        <Board/>
+        <Rack/>
+      </div>
       
     </div>
   );
