f078fb5261fcf36e0e9f142fde33e3964972d1a8
Jaynik Parsotomo <jp539@student.le.ac.uk>
2021-11-29 18:59:28 +0000

    Created a 2D array for the board, looped through it to create the corresponding square, return the board made of squares

--- scrabble-app/src/Components/Board.js
+++ scrabble-app/src/Components/Board.js
@@ -1,10 +1,40 @@
 import '../CSS/Board.css';
+import Square from './Square';
+
 
 const Board = () => {
+
+    const boardMap = [
+        ['TW','B','B','DL','B','B','B','TW','B','B','B','DL','B','B','TW'],
+        ['B','DW','B','B','B','TL','B','B','B','TL','B','B','B','DW','B'],
+        ['B','B','DW','B','B','B','DL','B','DL','B','B','B','DW','B','B'],
+        ['DL','B','B','DW','B','B','B','DL','B','B','B','DW','B','B','DL'],
+        ['B','B','B','B','DW','B','B','B','B','B','DW','B','B','B','B'],
+        ['B','TL','B','B','B','TL','B','B','B','TL','B','B','B','TL','B'],
+        ['B','B','DL','B','B','B','DL','B','DL','B','B','B','DL','B','B'],
+        ['TW','B','B','DL','B','B','B','C','B','B','B','DL','B','B','TW'],
+        ['B','B','DL','B','B','B','DL','B','DL','B','B','B','DL','B','B'],
+        ['B','TL','B','B','B','TL','B','B','B','TL','B','B','B','TL','B'],
+        ['B','B','B','B','DW','B','B','B','B','B','DW','B','B','B','B'],
+        ['DL','B','B','DW','B','B','B','DL','B','B','B','DW','B','B','DL'],
+        ['B','B','DW','B','B','B','DL','B','DL','B','B','B','DW','B','B'],
+        ['B','DW','B','B','B','TL','B','B','B','TL','B','B','B','DW','B'],
+        ['TW','B','B','DL','B','B','B','TW','B','B','B','DL','B','B','TW']
+    ];
+    let board = [];
+
+    for(let i = 0; i < boardMap.length; i++){
+        let a = boardMap[i];
+        for(let j = 0; j < 15; j++){
+            let val = a[j];
+            console.log(val);
+            board.push(<Square squareType = {val}/>);
+        }
+    }
+
+
     return (
-        <div id="scrabble-board">
-            
-        </div>
+        <div id="scrabble-board">{board}</div>
     );
 }
  
