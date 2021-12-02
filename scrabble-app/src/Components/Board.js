97414129f57189cc5cdf3c460aa69b04014166f3
Jaynik Parsotomo <jp539@student.le.ac.uk>
2021-12-02 14:51:26 +0000

    Added coordinates to each square as a unique ID

--- scrabble-app/src/Components/Board.js
+++ scrabble-app/src/Components/Board.js
@@ -3,7 +3,7 @@ import Square from './Square';
 
 
 const Board = () => {
-
+    //A 2D array containing each type of square their position
     const boardMap = [
         ['TW','B','B','DL','B','B','B','TW','B','B','B','DL','B','B','TW'],
         ['B','DW','B','B','B','TL','B','B','B','TL','B','B','B','DW','B'],
@@ -21,14 +21,23 @@ const Board = () => {
         ['B','DW','B','B','B','TL','B','B','B','TL','B','B','B','DW','B'],
         ['TW','B','B','DL','B','B','B','TW','B','B','B','DL','B','B','TW']
     ];
-    let board = [];
 
+    //Array to store each square of the board
+    let board = [];
+    
+    //Loop over the board squares to create the correpsonding square
     for(let i = 0; i < boardMap.length; i++){
         let a = boardMap[i];
         for(let j = 0; j < 15; j++){
             let val = a[j];
-            console.log(val);
-            board.push(<Square squareType = {val}/>);
+            //console.log(val);
+
+            //Create the coordinates of each square
+            let coords = i+","+j;
+            //console.log(coords);
+            //Save it to the array
+            //Creating an square passing the type and coordinates
+            board.push(<Square squareType = {val} coords = {coords}/>);
         }
     }
 
--- scrabble-app/src/Components/Square.js
+++ scrabble-app/src/Components/Square.js
@@ -3,24 +3,26 @@ import '../CSS/Square.css';
 
 const Square = (props) => {
 
+    //Switch case to create the different bonus squares
+    //Each square has a unique id which is the coordinates
     switch(props.squareType) {
         case 'B':
-            return <div className="square blank"></div>;
+            return <div id={props.coords} className="square blank"></div>;
    
         case 'TW':
-            return <div className="square tw">TW</div>;
+            return <div id={props.coords} className="square tw">TW</div>;
 
         case 'DW':
-                return <div className="square dw">DW</div>;
+                return <div id={props.coords} className="square dw">DW</div>;
 
         case 'TL':
-            return <div className="square tl">TL</div>;
+            return <div id={props.coords} className="square tl">TL</div>;
             
         case 'DL':
-            return <div className="square dl">DL</div>;
+            return <div id={props.coords} className="square dl">DL</div>;
         
         case 'C':
-            return <div className="square star">DW</div>;
+            return <div id={props.coords} className="square star">DW</div>;
 
     }
 
