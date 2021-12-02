e2b82e62844e4b6417fee79eef8fd512e9121504
Jaynik Parsotomo <jp539@student.le.ac.uk>
2021-12-02 19:32:49 +0000

    Rack access the letterdata class to get the letters for the starting rack

--- scrabble-app/src/Components/Rack.js
+++ scrabble-app/src/Components/Rack.js
@@ -1,8 +1,35 @@
 import "../CSS/Rack.css"
+import Tile from "./Tile";
+import Letters from "../WordList/letters.json";
+import LetterData from '../Helper/LetterData';
+import GenerateMove from "../Helper/GenerateMoves";
+
+
+
+//import LetterData from "../Helper/LetterData";
 
 const Rack = () => {
+    var letterData = new LetterData();
+    let startingRack = letterData.randomLetters(7);
+    //console.log(startingRack);
+
+    var generatMove = new GenerateMove();
+    let word = generatMove.generateWords(startingRack);
+    console.log(word);
+
+    let rackLetters = [];
+
+    for(let i = 0; i  < startingRack.length; i++){
+       let rackTile = startingRack[i];
+       //console.log(rackTile);
+       //rackLetters.push(<Tile letter = {rackTile}/>)
+    }
+   
+   
+  
     return (
         <div className="rack">
+            
 
         </div>
     );
