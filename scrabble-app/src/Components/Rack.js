b59333ac95447a10aa6393707aa522dffe85809b
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-01-25 21:36:13 +0000

    Generates the rack letters using letter data and passes the data to tile

--- scrabble-app/src/Components/Rack.js
+++ scrabble-app/src/Components/Rack.js
@@ -3,35 +3,40 @@ import Tile from "./Tile";
 import Letters from "../WordList/letters.json";
 import LetterData from '../Helper/LetterData';
 import GenerateMove from "../Helper/GenerateMoves";
+import GameData from "../JSONData/GameData.json"
 
 
 
 //import LetterData from "../Helper/LetterData";
 
 const Rack = () => {
-    var letterData = new LetterData();
-    let startingRack = letterData.randomLetters(7);
-    //console.log(startingRack);
+    var playerRack = [];
+    if(GameData.playerRack.length === 0){
+        var letterData = new LetterData();
+        playerRack = letterData.randomLetters(7);
+        //console.log(startingRack);
 
-    var generatMove = new GenerateMove();
-    let word = generatMove.generateWords(startingRack);
-    console.log(word);
+    }
+
+    //var generatMove = new GenerateMove();
+    //let word = generatMove.generateWords(startingRack);
+    
+    //console.log(word);
 
     let rackLetters = [];
 
-    for(let i = 0; i  < startingRack.length; i++){
-       let rackTile = startingRack[i];
-       //console.log(rackTile);
-       //rackLetters.push(<Tile letter = {rackTile}/>)
+   
+    //console.log(rackTile);
+    for(let i = 0; i < playerRack.length; i++){
+        rackLetters.push(<Tile letter = {playerRack[i]}/>);
     }
+   
+   
    
    
   
     return (
-        <div className="rack">
-            
-
-        </div>
+        <div className="rack">{rackLetters}</div>
     );
 }
  
