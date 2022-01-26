a1f2b23a7753431b41c237b32a927ab97b58d24f
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-01-26 19:51:53 +0000

    Changed the way the random letter is selected by using the JSON and delete the letter from the json array

--- scrabble-app/src/Helper/LetterData.js
+++ scrabble-app/src/Helper/LetterData.js
@@ -6,27 +6,37 @@ export default class LetterData{
     randomLetters = (num) => {
         let rack = [];
         //var properties = Object.getOwnPropertyNames(Letters);
-        let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+        //let letters = Letters.letters;
+       
+    
+
+        //let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
         //console.log(properties);
         for(let i = 0; i < num; i++){
             //const randIndex = properties.length * Math.random() << 0;
-            let randomLetter = letters.charAt(Math.floor(Math.random() * letters.length));
+            let randomLetter = Letters.letters[Math.floor(Math.random() * Letters.letters.length)];
             //console.log(randIndex);
             //const randLetter = properties[randIndex];
            //console.log("rl:", randomLetter);
+            //console.log("Random Letter", randomLetter);
             let noOfLetter = Letters[randomLetter].number;
-            console.log("Current:", noOfLetter);
+            //console.log("Current:", noOfLetter);
             Letters[randomLetter].number = noOfLetter - 1;
             noOfLetter = Letters[randomLetter].number;
 
             if(noOfLetter == 0){
-                letters = letters.replace(randomLetter,'');
+                let index = Letters.letters.indexOf(randomLetter);
+                Letters.letters.splice(index, 1);
+               // Letters.letters.replace(randomLetter,"");
+                //letters = letters.replace(randomLetter,'');
+                //letters = Letters.letters;
                 //console.log("rl:", randomLetter);
                 //console.log("l:", letters);
             }
             
             rack.push(randomLetter);
-            console.log("New:", noOfLetter);
+            //console.log("New:", noOfLetter);
+            //console.log(Letters.letters);
         }
         return rack;
    
