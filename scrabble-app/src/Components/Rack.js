c79d44cf129c8a9cfe0e7513b69976ecc4f3eaf5
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-01-26 19:41:26 +0000

    Changed the code to use a useState so in future rack can be updated in the dom dynamically

--- scrabble-app/src/Components/Rack.js
+++ scrabble-app/src/Components/Rack.js
@@ -4,39 +4,60 @@ import Letters from "../WordList/letters.json";
 import LetterData from '../Helper/LetterData';
 import GenerateMove from "../Helper/GenerateMoves";
 import GameData from "../JSONData/GameData.json"
-
-
-
-//import LetterData from "../Helper/LetterData";
+import { useState } from "react";
 
 const Rack = () => {
-    var playerRack = [];
-    if(GameData.playerRack.length === 0){
-        var letterData = new LetterData();
-        playerRack = letterData.randomLetters(7);
-        //console.log(startingRack);
-
+    const [currentPlayerRack, setPlayerRack] = useState(GameData.playerRack);
+    var newTiles;
+    if(currentPlayerRack.length < 7){
+        let letterData = new LetterData();
+        let noOfTilesNeeded = 7 - currentPlayerRack.length;
+        newTiles = letterData.randomLetters(noOfTilesNeeded);  
+        var newRack = currentPlayerRack.concat(newTiles);
+        GameData.playerRack = newRack;
+        setPlayerRack(newRack);
     }
+   
+    let rackTiles = [];
+    for(let i = 0; i < currentPlayerRack.length; i++){
+         rackTiles.push(<Tile key={i} letter = {currentPlayerRack[i]}/>);
+    }
+
+   
+   
+  
+   
+    
+    
+    // var playerRack = [];
+    // console.log("Current Player",GameData.playerRack);
+    // if(GameData.playerRack.length === 0){
+    //     var letterData = new LetterData();
+    //     playerRack = letterData.randomLetters(7);
+    //     console.log("rack",playerRack);
+    // }
 
     //var generatMove = new GenerateMove();
     //let word = generatMove.generateWords(startingRack);
     
     //console.log(word);
 
-    let rackLetters = [];
+    // let rackLetters = [];
+   
 
    
-    //console.log(rackTile);
-    for(let i = 0; i < playerRack.length; i++){
-        rackLetters.push(<Tile key={i} letter = {playerRack[i]}/>);
-    }
+    // //console.log(rackTile);
+    // for(let i = 0; i < playerRack.length; i++){
+    //     console.log(playerRack[i])
+    //     rackLetters.push(<Tile key={i} letter = {playerRack[i]}/>);
+    // }
    
    
    
    
   
     return (
-        <div className="rack">{rackLetters}</div>
+        <div className="rack">{rackTiles}</div>
     );
 }
  
--- scrabble-app/src/Components/Tile.js
+++ scrabble-app/src/Components/Tile.js
@@ -1,10 +1,18 @@
 import '../CSS/Tile.css';
 import Letters from '../WordList/letters.json';
+import { useState } from 'react';
 
 const Tile = (props) => {
-   return <div id={props.letter} className="tile">
-       <span className='tileLetter'>{props.letter}<span className='tileNum'>{Letters[props.letter].points}</span></span>
+    //const letter = props.letter;
+    console.log(props.letter);
+    const [letter, setTile] = useState(props.letter);
+    console.log(Letters[props.letter].number);
+
+   return( 
+   <div id={letter} className="tile">
+       <span className='tileLetter'>{letter}<span className='tileNum'>{Letters[letter].points}</span></span>
    </div>
+   );
 
 }
 
