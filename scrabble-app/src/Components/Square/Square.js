9c35a5f9267dabc4488e504f95a510c398273606
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-03 22:56:10 +0000

    To create the squares, switch statement is no longer being used, changed to using react hooks useState and useEffect, to update the squares when there is a change

--- scrabble-app/src/Components/Square/Square.js
+++ scrabble-app/src/Components/Square/Square.js
@@ -1,31 +1,32 @@
-import '../CSS/Square.css';
-
+import { useState, useEffect } from "react";
+import '../../StyleSheets/Square.css';
 
 const Square = (props) => {
+    const [letter, setLetter] = useState('');
+    const [squareType, setSquareType] = useState('B');
 
-    //Switch case to create the different bonus squares
-    //Each square has a unique id which is the coordinates
-    switch(props.squareType) {
-        case 'B':
-            return <div id={props.coords} className="square blank"></div>;
-   
-        case 'TW':
-            return <div id={props.coords} className="square tw">TW</div>;
+    useEffect(() => {
+        setSquareType(props.squareType)
+    }, [props.squareType]);
 
-        case 'DW':
-                return <div id={props.coords} className="square dw">DW</div>;
-
-        case 'TL':
-            return <div id={props.coords} className="square tl">TL</div>;
-            
-        case 'DL':
-            return <div id={props.coords} className="square dl">DL</div>;
-        
-        case 'C':
-            return <div id={props.coords} className="square star">DW</div>;
+    var bonus;
+    if(squareType === "B"){
+        bonus = "";
+    }
+    else if(squareType === "C"){
+        bonus = "DW";
 
     }
+    else{
+        bonus = squareType;
+    }
+    return(
+
+        <div id={props.coords} className={`square ${squareType}`}>
+            {letter || bonus}
+        </div>
+    )
 
 }
- 
+
 export default Square;
