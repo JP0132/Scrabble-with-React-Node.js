b9eb070afbb645f587362bf78364d47b3a2614b4
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-03 23:44:16 +0000

    The tiles are now updated using useState and useEffect

--- scrabble-app/src/Components/Tile/Tile.js
+++ scrabble-app/src/Components/Tile/Tile.js
@@ -1,18 +1,18 @@
-import '../CSS/Tile.css';
-import Letters from '../WordList/letters.json';
-import { useState } from 'react';
+import LetterData from '../../JSONData/LetterData.json';
+import { useState, useEffect } from 'react';
+import '../../StyleSheets/Tile.css'
 
-const Tile = (props) => {
-    //const letter = props.letter;
-    console.log(props.letter);
-    const [letter, setTile] = useState(props.letter);
-    console.log(Letters[props.letter].number);
+const Tile = ({letter}) => {
+    const [tileData, setTile] = useState("");
+    useEffect(() =>{
+        setTile(letter);
+    },[letter]);
 
-   return( 
-   <div id={letter} className="tile">
-       <span className='tileLetter'>{letter}<span className='tileNum'>{Letters[letter].points}</span></span>
-   </div>
-   );
+    return(
+        <div id={letter} className='tile'>
+            <span className='tileLetter'>{letter}<span className='tileNum'>{LetterData[letter].points}</span></span>
+        </div>
+    )
 
 }
 
