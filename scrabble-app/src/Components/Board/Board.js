6b42b8e30d948fafb80b71fdb6b5f1bdd2273008
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-03 22:54:49 +0000

    Change the imports and added commented out code for future reference to change the board using react hooks usestate and useeffect

--- scrabble-app/src/Components/Board/Board.js
+++ scrabble-app/src/Components/Board/Board.js
@@ -1,9 +1,8 @@
-import '../CSS/Board.css';
-import Square from './Square';
-
+import '../../StyleSheets/Board.css';
+import Square from '../Square/Square'
+import { useState, useEffect } from "react";
 
 const Board = () => {
-    //A 2D array containing each type of square their position
     const boardMap = [
         ['TW','B','B','DL','B','B','B','TW','B','B','B','DL','B','B','TW'],
         ['B','DW','B','B','B','TL','B','B','B','TL','B','B','B','DW','B'],
@@ -22,29 +21,31 @@ const Board = () => {
         ['TW','B','B','DL','B','B','B','TW','B','B','B','DL','B','B','TW']
     ];
 
-    //Array to store each square of the board
+    // const [currentBoard, setBoard] = useState(boardMap);
+    // useEffect(() => {
+
+    // });
+
     let board = [];
-    
-    //Loop over the board squares to create the correpsonding square
     for(let i = 0; i < boardMap.length; i++){
         let a = boardMap[i];
         for(let j = 0; j < 15; j++){
             let val = a[j];
-            //console.log(val);
-
+           
             //Create the coordinates of each square
             let coords = i+","+j;
-            //console.log(coords);
+        
             //Save it to the array
             //Creating an square passing the type and coordinates
             board.push(<Square key={coords} squareType = {val} coords = {coords}/>);
         }
     }
 
-
-    return (
+    return(
         <div id="scrabble-board">{board}</div>
-    );
+    )
+
+    
 }
- 
+
 export default Board;
