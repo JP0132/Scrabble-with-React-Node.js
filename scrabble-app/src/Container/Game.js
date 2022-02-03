52b123447d46615cf3203af7869d8de0fa438b9f
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-03 23:59:45 +0000

    Added the rack and playbtn components. A function passed as prop to playbtn that handles the click, currently only updates turnplayer to computer

--- scrabble-app/src/Container/Game.js
+++ scrabble-app/src/Container/Game.js
@@ -1,15 +1,31 @@
 import './Game.css';
 import Board from '../Components/Board/Board';
-
+import Rack from '../Components/Rack/Rack';
+import PlayBtn from '../Components/PlayBtn/PlayBtn';
+import GameData from '../JSONData/GameData.json'
+import { useState } from 'react';
 
 
 const Game = (props) => {
-
+    const [isComputerTurn, setComputerTurn] = useState(false);
+    const handlePlayWord = () => {
+        GameData.turnPlayer = "computer";
+        console.log(GameData.turnPlayer);
+        setComputerTurn(true);
+    }
     return(
         <div className='game'>
             <h1>AIECHO SCRABBLE</h1>
+            { isComputerTurn && <div>My Turn</div> }
             <div className='centre'>
                 <Board key="board"/>
+                <div className='row'>
+                    <Rack key="rack"/>
+                    <div className='btnCon'>
+                        <PlayBtn key="playBtn" handlePlayWord={handlePlayWord}/>
+                    </div>
+                </div>
+                
             </div>
           
         </div>
