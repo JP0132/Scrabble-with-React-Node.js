feb843c99d95790a000afcbe4c3979b424b8180b
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-05 19:30:52 +0000

    Making a fetch call to the node server to send the rack to calculate a word

--- scrabble-app/src/Helper/ComputerMove.js
+++ scrabble-app/src/Helper/ComputerMove.js
@@ -1,30 +1,40 @@
 import {DrawTiles} from "../Components/Rack/RackHelper";
 import GameData from "../JSONData/GameData.json";
+import axios from "axios";
 
 
 export function MakeMove() {
     var newTiles;
     var compRack = GameData.computerRack;
+    var computerMove;
 
     if(compRack < 7){
         let noOfTilesNeeded = 7 - compRack.length;
         newTiles = DrawTiles(noOfTilesNeeded);
         var newRack = compRack.concat(newTiles);
-        GameData.playerRack = newRack;
+        GameData.computerRack = newRack;
+        compRack = newRack;
     }
-    
+    console.log(compRack);
+
+    var data = {
+        "rack" : compRack
+    }
+    fetch('http://localhost:3001/api/computerMove/',{
+        method: "post",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(data),
+     
+    })
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        console.log("DATA SENT", data);
+        computerMove = data;
+    })
 
 
-
-    // const [computerMove, setComputerMove] = useState([]);
-    // useEffect(() => {
-    //     fetch('http://localhost:3001/api/computerMove/').then(res => {
-    //        return res.json();
-    //     })
-    //     .then(data => {
-            
-
-    //     })
-
-    // }, [isComputerTurn]);
 }
