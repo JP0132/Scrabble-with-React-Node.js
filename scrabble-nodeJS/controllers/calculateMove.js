48b2f7b6e2d8d07e071aa5cfee80099119ac796b
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-14 22:18:24 +0000

    node func to store the nodes of the gaddag and prefix identifier. Loading the dictionary words by letter.

--- scrabble-nodeJS/controllers/calculateMove.js
+++ scrabble-nodeJS/controllers/calculateMove.js
@@ -6,12 +6,17 @@ exports.calculateMove = (req, res) => {
     console.log("Calculating Move");
     console.log(req.body.rack);
     let rack = req.body.rack;
-    //let g = new Gaddag("../scrabble-nodeJS/CollinsScrabbleWords(2019).txt");
+    let g = new Gaddag("../scrabble-nodeJS/CollinsScrabbleWords(2019).txt");
+
     
     res.json({
         "hello":"chris"
     })
 }
+
+
+
+
 
 /*
 Concept of GADDAG
@@ -21,61 +26,162 @@ Prefix is reversed
 GADDAG is based on a DAWG, which is based on a TRIE
 */
 
+const Node =  function (key) {
+    this.key = key;
+    this.parent = null;
+    this.children = {};
+    this.$ = 0;
+    this.getWord = function(){
+        let output = [];
+        let node = this;
+
+        while (node !== null) {
+            output.unshift(node.key);
+            node = node.parent;
+        }
+    }
+}
+
 class Gaddag{
     constructor (dict){
-        //fs.readFile(dict, (err, data) => loadDictionary(err, data));
-        readline(dict);
+        //fs.readFile(dict, (err, data) => this.loadDictionary(err, data));
+        this.root = new Node(null);
+        this.previousWord = '';
+        this.uncheckedNodes = [];
+        
+
+        this.readLine(dict);
+        
     }
     
     readLine(dict){
-        const fileStream = fs.createReadStream(dict);
-
-        const rl = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity
+        console.log("hi");
+        var lineReader = require('readline').createInterface({
+            input: require('fs').createReadStream(dict)
+        });
+        const words = {
+            A: [],
+            B: [],
+            C: [],
+            D: [],
+            E: [],
+            F: [],
+            G: [],
+            H: [],
+            I: [],
+            J: [],
+            K: [],
+            L: [],
+            M: [],
+            N: [],
+            O: [],
+            P: [],
+            Q: [],
+            R: [],
+            S: [],
+            T: [],
+            U: [],
+            V: [],
+            W: [],
+            X: [],
+            Y: [],
+            Z: []
+        };
+          
+        lineReader.on('line', function (line) {
+           
+            const mapWord = (line) => {
+                return (letter, index, arr) =>
+                  words[letter].push(
+                    index === 0
+                    ? line
+                    : line.slice(index, line.length) + '_' + arr.slice(0, index).reverse().join(''));
+              };
+      
+            //line.split('').map(mapWord(line));
+            //lines[i].split('').map(mapWord(lines[i]));
+            
+            if(line === "FINALLINE"){
+                //console.log("A word", words["A"]);
+                console.log("word", words);
+    
+            }
+            else{
+                line.split('').map(mapWord(line));
+            }
+        
         });
 
-        for await (const line of rl) {
-            this.loadDictionary(line);
-        }
+
+        
 
     }
      
     loadDictionary(line){
+        console.log("h");
         const words = {
-            "A": [],
-            "B": [],
-            "C": [],
-            "D": [],
-            "E": [],
-            "F": [],
-            "G": [],
-            "H": [],
-            "I": [],
-            "J": [],
-            "K": [],
-            "L": [],
-            "M": [],
-            "N": [],
-            "O": [],
-            "P": [],
-            "Q": [],
-            "R": [],
-            "S": [],
-            "T": [],
-            "U": [],
-            "V": [],
-            "W": [],
-            "X": [],
-            "Y": [],
-            "Z": []
+            A: [],
+            B: [],
+            C: [],
+            D: [],
+            E: [],
+            F: [],
+            G: [],
+            H: [],
+            I: [],
+            J: [],
+            K: [],
+            L: [],
+            M: [],
+            N: [],
+            O: [],
+            P: [],
+            Q: [],
+            R: [],
+            S: [],
+            T: [],
+            U: [],
+            V: [],
+            W: [],
+            X: [],
+            Y: [],
+            Z: []
         };
-        const mapWord = (readLine) => {
-            let letter = readLine[0];
-            words[letter].push(readLine);
-        }
+        const mapWord = (line) => {
+            return (letter, index, arr) =>
+              words[letter].push(
+                index === 0
+                ? line
+                : line.slice(index, line.length) + '_' + arr.slice(0, index).reverse().join(''));
+          };
+  
+        //line.split('').map(mapWord(line));
+        //lines[i].split('').map(mapWord(lines[i]));
+        
+        if(line === "FINALLINE"){
+            //console.log("A word", words["A"]);
+            console.log("A word", words["A"]);
 
-        mapWord(line);
+        }
+        else{
+            console.log(line.split(''));
+            line.split('').map(mapWord(line));
+            console.log("A word", words["A"]);
+
+        }
+        
+  
+        // const mapWord = (readLine) => {
+        //     let letter = readLine[0];
+        //     for(let i = 0; i < readline.length; i++){
+
+
+        //     }
+        //     words[letter].push(readLine);
+
+        // }
+
+        // mapWord(line);
 
     }
 
