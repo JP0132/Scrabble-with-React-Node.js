6a074b9a4ead8f068fb4bb4e1289edbaf93e628e
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-27 21:31:43 +0000

    Using the react library can now move the tile from the rack to the board

--- scrabble-app/src/Components/Tile/Tile.js
+++ scrabble-app/src/Components/Tile/Tile.js
@@ -1,16 +1,30 @@
 import LetterData from '../../JSONData/LetterData.json';
 import { useState, useEffect } from 'react';
 import '../../StyleSheets/Tile.css'
+import { useDrag, useDrop } from 'react-dnd';
 
-const Tile = ({letter}) => {
+
+
+
+const Tile = (props) => { 
+    const [{ isDragging }, drag] =  useDrag(() => ({
+        type:  "tile",
+        item: {letterValue: props.letter},
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging(),
+        })
+    }))
     const [tileData, setTile] = useState("");
     useEffect(() =>{
-        setTile(letter);
-    },[letter]);
+        setTile(props.letter);
+    },[props.letter]);
+    //const opacity = isDragging ? 0 : 1;
+    //const tileCursor = isDragging ? "move" : "pointer";
 
     return(
-        <div id={letter} className='tile'>
-            <span className='tileLetter'>{letter}<span className='tileNum'>{LetterData[letter].points}</span></span>
+        
+        <div id={props.letter} className='tile' ref={drag} style={{opacity: isDragging ? 0 : 1 , cursor: isDragging ? "all-scroll" : "pointer"}}>
+            <span className='tileLetter'>{props.letter}<span className='tileNum'>{LetterData[props.letter].points}</span></span>
         </div>
     )
 
