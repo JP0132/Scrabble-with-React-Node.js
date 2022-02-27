f4cfc998c34dd6447349b67d3ba764a510b13a41
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-27 21:29:51 +0000

    Using the useDrop, placed the tiles on the board, need to position and delete tiles still

--- scrabble-app/src/Components/Square/Square.js
+++ scrabble-app/src/Components/Square/Square.js
@@ -1,9 +1,28 @@
 import { useState, useEffect } from "react";
 import '../../StyleSheets/Square.css';
+import { useDrop } from "react-dnd";
+import Tile from "../Tile/Tile";
+import GameData from '../../JSONData/GameData.json';
 
 const Square = (props) => {
     const [letter, setLetter] = useState('');
     const [squareType, setSquareType] = useState('B');
+
+    const [{isOver}, drop] = useDrop(() => ({
+        accept:"tile",
+        drop: (item) => addToSquare(item.letterValue),
+    }))
+
+    const addToSquare = (l) => {
+        setLetter(<Tile key={Math.random()} letter = {l}/>);
+        let currentRack = GameData.playerRack;
+        let index = currentRack.indexOf(l);
+        currentRack.splice(index, 1);
+        console.log(currentRack);
+        GameData.playerRack = currentRack;
+
+    }
+
 
     useEffect(() => {
         setSquareType(props.squareType)
@@ -21,9 +40,10 @@ const Square = (props) => {
         bonus = squareType;
     }
     return(
-
-        <div id={props.coords} className={`square ${squareType}`}>
-            {letter || bonus}
+        <div>
+            <div ref={drop} id={props.coords} className={`square ${squareType}`}>
+                {letter || bonus}
+            </div>
         </div>
     )
 
