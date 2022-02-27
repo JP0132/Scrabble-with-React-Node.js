69de756fca05aab08d31127987966f57782a0336
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-02-27 21:27:54 +0000

    Added drag and drop provider so the whole application has access to it

--- scrabble-app/src/App.js
+++ scrabble-app/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Game from './Container/Game';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 
 function App() {
   return (
+  <DndProvider backend={HTML5Backend}>
    <Game/>
+  </DndProvider>
    
   );
 }
