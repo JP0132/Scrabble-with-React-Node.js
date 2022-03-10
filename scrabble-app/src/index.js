3575889f3b06f3fe3a468a4939fb7793f99128f8
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-03-10 22:28:16 +0000

    Updated index to pass new redux store

--- scrabble-app/src/index.js
+++ scrabble-app/src/index.js
@@ -3,27 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from 'react-redux';
-import rackReducer from './features/rack';
+import { storeSlicer } from "./app/store" ;
 
-const store = configureStore({
-  reducer: {
-    rack: rackReducer,
-  },
-});
-
-import { StateProvider } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={storeSlicer}>
       <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+//export {store};
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
