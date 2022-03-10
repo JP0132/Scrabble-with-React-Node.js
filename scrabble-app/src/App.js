2d1d3194b981de36f0c19aff809fc2fd8ec334bf
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-03-10 22:37:59 +0000

    Updated packages and layout changes to App.js

--- scrabble-app/src/App.js
+++ scrabble-app/src/App.js
@@ -3,13 +3,12 @@ import Game from './Container/Game';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-
 function App() {
   return (
-  <DndProvider backend={HTML5Backend}>
-   <Game/>
-  </DndProvider>
-   
+    <DndProvider backend={HTML5Backend}>
+      <Game/>
+    </DndProvider>
+  
   );
 }
 
