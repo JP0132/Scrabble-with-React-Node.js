297e8e45178d10030bc499e88232fcc2bf7daf10
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-03-13 18:15:30 +0000

    added a the square slicer to the reducers for gloabal state access

--- scrabble-app/src/app/store.js
+++ scrabble-app/src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit"
-import rackReducer from "../features/rackSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import rackReducer from "../features/rackSlice";
+import squareReducer from "../features/squareSlice";
+
 export const storeSlicer = configureStore({
     reducer: {
         rack: rackReducer,
+        square: squareReducer
 
     }
 })
