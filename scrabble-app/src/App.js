b9f52a368757bff1fbdb739a5e4a35c34f2ea9e2
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-04-16 10:54:17 +0100

    Changes to app style, backaground is set to all the pages

--- scrabble-app/src/App.js
+++ scrabble-app/src/App.js
@@ -5,10 +5,9 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <Game/>
-    </DndProvider>
-  
+      <DndProvider backend={HTML5Backend}>
+        <Game/>
+      </DndProvider>
   );
 }
 
