a2d7ab70538c96cb9b3b1ff3e575cde9585cb4af
Jaynik Parsotomo <jp539@student.le.ac.uk>
2022-04-25 13:44:44 +0100

    Updated redux store containing the reducers

--- scrabble-app/src/app/store.js
+++ scrabble-app/src/app/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import boardReducer from "../features/boardSlice";
+import gameReducer from "../features/gameSlice";
 import rackReducer from "../features/rackSlice";
 import squareReducer from "../features/squareSlice";
+import tileBag from "../features/tilebagSlice";
 
 export const storeSlicer = configureStore({
     reducer: {
+        game: gameReducer,
+        board: boardReducer,
         rack: rackReducer,
-        square: squareReducer
-
+        square: squareReducer,
+        tilebag: tileBag,
     }
 })
